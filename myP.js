7b568bcd3c8ef1588b7dd1ca0672f4ab590e101f
Aaron <thatshakespeare2@gmail.com>
2024-06-21 03:03:50 +0100

    ok

--- myP.js
+++ myP.js
@@ -1,24 +1,29 @@
-const carouselInner = document.querySelector('.carousel-inner');
-const cards = document.querySelectorAll('.card');
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
+emailjs.init('_sDMwEekjXEiFCce2'); // Replace with your Public Key
 
-let currentCardIndex = 0;
+(function () {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-prevButton.addEventListener('click', () => {
-  currentCardIndex--;
-  updateCarousel();
-});
+    const fullName = document.getElementById('dark_field_name').value;
+    const email = document.getElementById('dark_field_email').value;
+    const message = document.getElementById('textarea_field').value;
 
-nextButton.addEventListener('click', () => {
-  currentCardIndex++;
-  updateCarousel();
-});
+    const templateParams = {
+      from_name: fullName,
+      from_email: email,
+      message: message,
+    };
 
-function updateCarousel() {
-  const cardWidth = cards[0].offsetWidth;
-  const translateX = currentCardIndex * cardWidth;
-  carouselInner.style.transform = `translateX(-${translateX}px)`;
-}
-
-updateCarousel();
+    emailjs
+      .send('service_wfb9o14', 'template_jal1dyd', templateParams) // Replace with your Service ID and Template ID
+      .then((response) => {
+        console.log('Email sent successfully!', response);
+        form.reset()
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error);
+        
+      });
+  });
+})();
