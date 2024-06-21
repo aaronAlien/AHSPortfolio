90e93d00244df903da8f9b272c86d813d5ba6355
Aaron <thatshakespeare2@gmail.com>
2024-06-21 03:18:00 +0100

    email confirm

--- myP.js
+++ myP.js
@@ -19,11 +19,21 @@ emailjs.init('_sDMwEekjXEiFCce2'); // Replace with your Public Key
       .send('service_wfb9o14', 'template_jal1dyd', templateParams) // Replace with your Service ID and Template ID
       .then((response) => {
         console.log('Email sent successfully!', response);
-        form.reset()
+        // Append a confirmation message to the DOM
+        const confirmationMessage = document.createElement('div');
+        confirmationMessage.classList.add('nes-container', 'is-dark', 'is-rounded');
+        confirmationMessage.innerHTML = 'Thank you for  you email! I will get back to you soon.';
+        confirmationMessage.style.marginTop('2rem');
+        confirmationMessage.style.color('green');
+        confirmationMessage.style.textTransform('upppercase');
+
+        const formDiv = document.getElementById('form-div');
+        formDiv.appendChild(confirmationMessage);
+
+        form.reset();
       })
       .catch((error) => {
         console.error('Error sending email:', error);
-        
       });
   });
 })();
